9603dd424691e98b36e94c31f068f2c793e0820c
henrymutega <hmutega@gmail.com>
2019-09-06 21:48:41 +0300

    Initial commit to the project.

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { GoalComponent } from './goal/goal.component';
+import { GoalDetailsComponent } from './goal-details/goal-details.component';
+import { StrikethroughDirective } from './strikethrough.directive';
+import { DateCountPipe } from './date-count.pipe';
+import { GoalFormComponent } from './goal-form/goal-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GoalComponent,
+    GoalDetailsComponent,
+    StrikethroughDirective,
+    DateCountPipe,
+    GoalFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
